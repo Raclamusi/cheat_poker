00d90572f8e2d82f1683e6b21257f349ca7027e4
raclamusi <raclamusi@gmail.com>
2022-01-09 16:02:19 +0900

    適当例外対策

--- index.js
+++ index.js
@@ -76,122 +76,128 @@ wsServer.on("connection", ws => {
     ws.on("message", data => {
         console.log(`[message] ${data}`);
         const parsedData = JSON.parse(data);
-        if (wsRoomMap.has(ws)) {
-            rooms.get(wsRoomMap.get(ws)).procedure(ws, parsedData.message, parsedData.data);
-        }
-        switch (parsedData.message) {
-            case "CREATE": {
-                if (idQueue.length === 0) {
-                    sendMessage(ws, "ERROR", "これ以上ルームを作成することはできません");
-                    break;
-                }
-                const roomInfo = parsedData.data;
-                roomInfo.name = roomInfo.name.replace(/\s+/g, " ").replace(/^ | $/g, "");
-                roomInfo.player = roomInfo.player.replace(/\s+/g, " ").replace(/^ | $/g, "");
-                if (roomInfo.name.length === 0) {
-                    sendMessage(ws, "ERROR", "ルーム名を入力する必要があります");
-                    break;
-                }
-                if (roomInfo.player.length === 0) {
-                    sendMessage(ws, "ERROR", "プレイヤー名を入力する必要があります");
-                    break;
-                }
-                if (Array.from(rooms.values()).find(e => e.name === roomInfo.name) !== undefined) {
-                    sendMessage(ws, "ERROR", `ルーム名「${roomInfo.name}」は既に使用されています`);
-                    break;
-                }
-                if (roomInfo.startChips === null || roomInfo.startChips <= 0) {
-                    sendMessage(ws, "ERROR", "開始時チップは正の正数である必要があります");
-                    break;
-                }
-                if (roomInfo.waitingTime === null || roomInfo.waitingTime <= 0) {
-                    sendMessage(ws, "ERROR", "ターン制限時間は正の正数である必要があります");
-                    break;
-                }
-                if (roomInfo.blindInterval === null || roomInfo.blindInterval <= 0) {
-                    sendMessage(ws, "ERROR", "ブラインド上昇間隔は正の正数である必要があります");
-                    break;
-                }
-                const id = idQueue[0];
-                idQueue.shift();
-                const room = new PokerRoom(
-                    id,
-                    roomInfo.name,
-                    roomInfo.discription,
-                    roomInfo.locked ? roomInfo.password : null,
-                    roomInfo.startChips,
-                    roomInfo.waitingTime,
-                    roomInfo.blindInterval,
-                    roomInfo.cheat,
-                    roomInfo.player,
-                    ws
-                );
-                rooms.set(id, room);
-                wsRoomMap.set(ws, id);
-                sendMessage(ws, "ENTERED", { room: room.getInfo(room.players[0]), name: room.name, id: id, player: roomInfo.player, reentry: room.players[0].reentry });
-                updateRooms();
-                break;
+        try {
+            if (wsRoomMap.has(ws)) {
+                rooms.get(wsRoomMap.get(ws)).procedure(ws, parsedData.message, parsedData.data);
             }
-            case "JOIN": {
-                const entry = parsedData.data;
-                entry.player = entry.player.replace(/\s+/g, " ").replace(/^ | $/g, "");
-                if (!rooms.has(entry.id)) {
-                    if (entry.reentry === undefined) {
-                        sendMessage(ws, "ERROR", "ルームに参加できません");
+            switch (parsedData.message) {
+                case "CREATE": {
+                    if (idQueue.length === 0) {
+                        sendMessage(ws, "ERROR", "これ以上ルームを作成することはできません");
+                        break;
                     }
-                    break;
-                }
-                if (entry.player.length === 0) {
-                    if (entry.reentry === undefined) {
+                    const roomInfo = parsedData.data;
+                    roomInfo.name = roomInfo.name.replace(/\s+/g, " ").replace(/^ | $/g, "");
+                    roomInfo.player = roomInfo.player.replace(/\s+/g, " ").replace(/^ | $/g, "");
+                    if (roomInfo.name.length === 0) {
+                        sendMessage(ws, "ERROR", "ルーム名を入力する必要があります");
+                        break;
+                    }
+                    if (roomInfo.player.length === 0) {
                         sendMessage(ws, "ERROR", "プレイヤー名を入力する必要があります");
+                        break;
                     }
+                    if (Array.from(rooms.values()).find(e => e.name === roomInfo.name) !== undefined) {
+                        sendMessage(ws, "ERROR", `ルーム名「${roomInfo.name}」は既に使用されています`);
+                        break;
+                    }
+                    if (roomInfo.startChips === null || roomInfo.startChips <= 0) {
+                        sendMessage(ws, "ERROR", "開始時チップは正の正数である必要があります");
+                        break;
+                    }
+                    if (roomInfo.waitingTime === null || roomInfo.waitingTime <= 0) {
+                        sendMessage(ws, "ERROR", "ターン制限時間は正の正数である必要があります");
+                        break;
+                    }
+                    if (roomInfo.blindInterval === null || roomInfo.blindInterval <= 0) {
+                        sendMessage(ws, "ERROR", "ブラインド上昇間隔は正の正数である必要があります");
+                        break;
+                    }
+                    const id = idQueue[0];
+                    idQueue.shift();
+                    const room = new PokerRoom(
+                        id,
+                        roomInfo.name,
+                        roomInfo.discription,
+                        roomInfo.locked ? roomInfo.password : null,
+                        roomInfo.startChips,
+                        roomInfo.waitingTime,
+                        roomInfo.blindInterval,
+                        roomInfo.cheat,
+                        roomInfo.player,
+                        ws
+                    );
+                    rooms.set(id, room);
+                    wsRoomMap.set(ws, id);
+                    sendMessage(ws, "ENTERED", { room: room.getInfo(room.players[0]), name: room.name, id: id, player: roomInfo.player, reentry: room.players[0].reentry });
+                    updateRooms();
                     break;
                 }
-                const room = rooms.get(entry.id);
-                if (entry.reentry !== undefined) {
-                    const player = room.players.find(e => e.name === entry.player);
-                    if (player === undefined || player.online || player.reentry !== entry.reentry) {
+                case "JOIN": {
+                    const entry = parsedData.data;
+                    entry.player = entry.player.replace(/\s+/g, " ").replace(/^ | $/g, "");
+                    if (!rooms.has(entry.id)) {
+                        if (entry.reentry === undefined) {
+                            sendMessage(ws, "ERROR", "ルームに参加できません");
+                        }
                         break;
                     }
-                    wsRoomMap.delete(player.ws);
-                    player.ws = ws;
-                    player.resetReentry();
-                    room.connect(ws);
-                }
-                else {
-                    if (room.password !== null && entry.password !== room.password) {
-                        sendMessage(ws, "ERROR", "パスワードが違います");
+                    if (entry.player.length === 0) {
+                        if (entry.reentry === undefined) {
+                            sendMessage(ws, "ERROR", "プレイヤー名を入力する必要があります");
+                        }
                         break;
                     }
-                    if (room.players.some(e => e.name === entry.player)) {
-                        sendMessage(ws, "ERROR", `プレイヤー名「${entry.player}」はこのルームで既に使われています`);
-                        break;
+                    const room = rooms.get(entry.id);
+                    if (entry.reentry !== undefined) {
+                        const player = room.players.find(e => e.name === entry.player);
+                        if (player === undefined || player.online || player.reentry !== entry.reentry) {
+                            break;
+                        }
+                        wsRoomMap.delete(player.ws);
+                        player.ws = ws;
+                        player.resetReentry();
+                        room.connect(ws);
                     }
-                    room.join(entry.player, ws);
-                }
-                wsRoomMap.set(ws, entry.id);
-                const player = room.players.find(e => e.ws === ws);
-                sendMessage(ws, "ENTERED", { room: room.getInfo(player), name: room.name, id: entry.id, player: entry.player, reentry: player.reentry });
-                updateRooms();
-                break;
-            }
-            case "LEAVE": {
-                if (!wsRoomMap.has(ws)) break;
-                const id = wsRoomMap.get(ws);
-                if (!rooms.has(id)) break;
-                const room = rooms.get(id);
-                if (room.empty()) {
-                    rooms.delete(id);
-                    wsRoomMap.forEach((v, k, m) => {
-                        if (v === id) m.delete(k);
-                    });
+                    else {
+                        if (room.password !== null && entry.password !== room.password) {
+                            sendMessage(ws, "ERROR", "パスワードが違います");
+                            break;
+                        }
+                        if (room.players.some(e => e.name === entry.player)) {
+                            sendMessage(ws, "ERROR", `プレイヤー名「${entry.player}」はこのルームで既に使われています`);
+                            break;
+                        }
+                        room.join(entry.player, ws);
+                    }
+                    wsRoomMap.set(ws, entry.id);
+                    const player = room.players.find(e => e.ws === ws);
+                    sendMessage(ws, "ENTERED", { room: room.getInfo(player), name: room.name, id: entry.id, player: entry.player, reentry: player.reentry });
                     updateRooms();
+                    break;
                 }
-                wsRoomMap.delete(ws);
-                sendMessage(ws, "EXITED", {});
-                updateRooms();
-                break;
+                case "LEAVE": {
+                    if (!wsRoomMap.has(ws)) break;
+                    const id = wsRoomMap.get(ws);
+                    if (!rooms.has(id)) break;
+                    const room = rooms.get(id);
+                    if (room.empty()) {
+                        rooms.delete(id);
+                        wsRoomMap.forEach((v, k, m) => {
+                            if (v === id) m.delete(k);
+                        });
+                        updateRooms();
+                    }
+                    wsRoomMap.delete(ws);
+                    sendMessage(ws, "EXITED", {});
+                    updateRooms();
+                    break;
+                }
             }
+        }
+        catch (e) {
+            console.error(e);
+            sendMessage(ws, "ERROR", "内部エラーが発生しました");
         }
     });
 
